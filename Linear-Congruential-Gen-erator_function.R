b6bf3b1b89a463317afc8f694817bba1d7b56bdd
Ryan Tran <trankhang2856@gmail.com>
2026-08-14 14:02:09 +0700

    global: first commit with LCG function with 2 type: single result and table

---
+++ Linear-Congruential-Gen-erator_function.R
@@ -0,0 +1,44 @@
+#Linear Congruential Generator
+  
+#hàm đơn
+LCG = function(X0, a, n, c, m ){
+    
+    X = numeric(n+1)
+    X[1] = X0
+    for (i in 2:(n + 1) ){
+      X[i] = (a*X[i-1]+c)%%m
+    }
+    return(X)
+  }
+  
+LCG(3, 7, 20, 4, 3)
+
+lcg_table = function(X0, a, n, c, m ){
+
+  stt = 1:(n+1)
+  X = numeric(n+1)
+  X[1] = X0
+  equation = numeric(n+1)
+  equation[1] = a * X0 + c
+  
+  for (i in 2:(n+1) ){
+    X[i] = (a * X[i-1] + c) %% m
+  }
+
+  
+  for (j in 2:(n+1) ){
+    equation[j] = (a * X[j-1] + c)
+    
+  }
+  
+  result = data.frame(
+    n = stt,
+    'a*X(n-1) + c' = equation,
+    'Xn' = X
+  )
+  
+  return(result)
+}
+  
+lcg_table(3, 7, 20, 4, 3)
+  
