9b4d74becfdbc28422f9bc050a90daca18896491
Ryan Tran <trankhang2856@gmail.com>
2026-08-22 00:52:09 +0700

    feat: add title for lab/R/UNI_D.R

--- lab/R/UNI_D.R
+++ lab/R/UNI_D.R
@@ -1,18 +1,21 @@
+## Example 4, Section 1.2
+
 Uni_distribution = function(X){
   A = runif(1)
-  cat('Số tạo ngẫu nhiên: ',A,'\n')
+  cat('Số tạo ngẫu nhiên: x =',A,'\n')
   sum_range = cumsum(X)
 
   for (i in 1:length(X)){
     if (A <= sum_range[i]){
+      cat('x thuộc khoảng:','[',sum_range[i-1], ',',sum_range[i],']','\n')
       return(i)
     }
 
   }
   
-  return(length(X))
+  cat('khoảng thứ:',(length(X)))
 }
 
-X = c(1/10, 2/10, 7/10)
+X = c(1/10, 2/10, 4/10, 3/10)
 
 Uni_distribution(X)
